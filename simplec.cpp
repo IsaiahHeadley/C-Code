7fe4d07906a1e8391a425c6cbb30e1deb9300e25
Isaiah Headley <36669288+IsaiahHeadley@users.noreply.github.com>
2019-02-11 10:18:48 -0500

    Add files via upload

---
+++ simplec.cpp
@@ -0,0 +1,17 @@
+// Simple C++ program to display output from the console 
+
+// Header file for input output functions 
+#include<iostream> 
+
+using namespace std; 
+
+// main function - int main()
+// where the execution of program begins 
+
+int main() 
+{ 
+	// prints to the console 
+	cout<<"This is a simple C++ program"; 
+	
+	return 0; 
+} 
