b7ca6c8953c9ded4995e8f97e1512ed4b7cadf14
Isaiah Headley <36669288+IsaiahHeadley@users.noreply.github.com>
2020-12-10 10:25:21 -0500

    Update and rename main.cpp to array.cpp

--- array.cpp
+++ array.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+// this is an array displayed to the console using a for loop in cpp
 
 int main() {
+#include <iostream>
+
   int myArray[4] ={1, 2, 3, 4};
 
   for(int x = 0; x < 4; x++){
